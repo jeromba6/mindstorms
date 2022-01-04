5ed7593366c017e269e3bf87bca82e32e2293592
Jeroen van Gemert <jeroen.vangemert@kpn.com>
2022-01-04 11:42:31 +0100

    Added display2

---
+++ display2.py
@@ -0,0 +1,39 @@
+import hub
+import time
+
+def turn_left(image):
+    new_image = ['']*5
+    for line in image:
+        for i in range(5):
+            new_image[4-i] += line[i]
+    return new_image
+
+def turn_upside_down(image):
+    new_image = turn_left(image)
+    return turn_left(new_image)
+
+def turn_right(image):
+    new_image = turn_left(image)
+    new_image = turn_left(new_image)
+    return turn_left(new_image)
+
+image=[
+    '00900',
+    '09990',
+    '99999',
+    '00900',
+    '00900'
+]
+while True:
+    img=hub.Image(':'.join(image))
+    hub.display.show(img)
+    time.sleep(1)
+    img=hub.Image(':'.join(turn_right(image)))
+    hub.display.show(img)
+    time.sleep(1)
+    img=hub.Image(':'.join(turn_upside_down(image)))
+    hub.display.show(img)
+    time.sleep(1)
+    img=hub.Image(':'.join(turn_left(image)))
+    hub.display.show(img)
+    time.sleep(1)
